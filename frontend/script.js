862257bda06f80fdd25bf2eff079b37055ec1856
Caner Erdem <senin-github-emailin@example.comecaner670@gmail.com>
2026-08-21 09:49:29 +0300

    Add movie search feature

--- frontend/script.js
+++ frontend/script.js
@@ -22,3 +22,40 @@ fetch('http://localhost:3000/movies')
     .catch(error => {
         console.error('Film verileri alınamadı:', error);
     });
+
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const noResults = document.getElementById('no-results');
+
+searchButton.addEventListener('click', () => {
+    const searchText = searchInput.value.toLowerCase();
+const movieCards = document.querySelectorAll('.movie-card');
+
+if (searchText.trim() === '') {
+    movieCards.forEach(card => {
+        card.style.display = 'block';
+    });
+
+    noResults.style.display = 'none';
+    return;
+}
+
+    let found = false;
+
+    movieCards.forEach(card => {
+        const title = card.querySelector('h3').textContent.toLowerCase();
+
+        if (title.includes(searchText)) {
+            card.style.display = 'block';
+            found = true;
+        } else {
+            card.style.display = 'none';
+        }
+    });
+
+    if (found) {
+        noResults.style.display = 'none';
+    } else {
+        noResults.style.display = 'block';
+    }
+});
