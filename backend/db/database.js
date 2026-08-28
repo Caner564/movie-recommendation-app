bde9f1fef5b338f65cf46ba919f7dce6023278d8
Caner Erdem <senin-github-emailin@example.comecaner670@gmail.com>
2026-08-28 17:26:43 +0300

    Complete project features and final improvements

--- backend/db/database.js
+++ backend/db/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'asdqwrdcxz142',
+    password: 'YOUR_MYSQL_PASSWORD',
     database: 'movie_recommendation'
 });
 
